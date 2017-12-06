8e0673d5e12bf016280e9fcdd7191d3b1ef9a6bb
Kritchai Phromros <ky@pixka.me>
2017-12-06 07:37:21 +0700

    Support ds18sensor by user_id

--- src/main/kotlin/me/pixka/kt/pibase/r/Ds18sensorRepo.kt
+++ src/main/kotlin/me/pixka/kt/pibase/r/Ds18sensorRepo.kt
@@ -13,7 +13,11 @@ interface Ds18sensorRepo : JpaRepository<DS18sensor, Long> {
     fun findByName(n: String): DS18sensor
 
     @Query("from DS18sensor d where d.name like %?1% or d.call like %?1%")
-    fun search(s: String, page: Pageable?): List<DS18sensor>
+    fun search(s: String, page: Pageable?): List<DS18sensor>?
+
+
+    @Query("from DS18sensor d where (d.name like %?1% or d.call like %?1%) and d.user_id = ?2")
+    fun search(s: String,uid:Long, page: Pageable?): List<DS18sensor>?
 
     fun findTop1ByForreadOrderByLasteditDesc(b: Boolean): DS18sensor
 
