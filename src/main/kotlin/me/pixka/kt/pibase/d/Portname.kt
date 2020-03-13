a17d5032bec38f80f25fdc396e35b1785c7a47a3
Kritchai <ky@pixka.me>
2020-03-13 18:36:45 +0700

    Add enable

--- src/main/kotlin/me/pixka/kt/pibase/d/Portname.kt
+++ src/main/kotlin/me/pixka/kt/pibase/d/Portname.kt
@@ -17,7 +17,7 @@ import javax.persistence.ManyToOne
 @Entity
 @Cacheable
 @Cache(usage = org.hibernate.annotations.CacheConcurrencyStrategy.READ_WRITE)
-class Portname(var name: String? = null, var piport: String? = null, var refid: Long? = null,
+class Portname(var name: String? = null, var piport: String? = null, var refid: Long? = null,var portenable:Boolean?=false,
                @ManyToOne var portmode: Portmode? = null,
                @Column(insertable = false, updatable = false) var portmode_id: Long? = null,
                var checkversion:Long?=0) : En() {
