b746ad10509a04a422da820f3d2ddda824d1a1ac
Kritchai Phromros <ky@pixka.me>
2020-10-06 18:26:33 +0700

    Fix bug thaifont

--- src/main/kotlin/me/pixka/kt/pibase/s/HttpService.kt
+++ src/main/kotlin/me/pixka/kt/pibase/s/HttpService.kt
@@ -1,23 +1,28 @@
 package me.pixka.kt.pibase.s
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import me.pixka.kt.pibase.t.HttpGetTask
 import org.springframework.stereotype.Service
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 import java.nio.charset.Charset
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 @Service
-class HttpService (){
+class HttpService() {
     val om = ObjectMapper()
 
     @Throws(Exception::class)
-    fun get(url: String, timeout: Int ): String {
+    fun get(url: String, timeout: Int): String {
         try {
 //            println("call")
             var url = URL(url)
             var c = url.openConnection() as HttpURLConnection
+            c.setRequestProperty("Accept-Charset", "UTF-8");
+            c.setRequestProperty("Accept", "application/json")
             c.requestMethod = "GET"
             c.connectTimeout = timeout
             c.readTimeout = timeout
@@ -31,7 +36,7 @@ class HttpService (){
     fun readrespone(c: HttpURLConnection): StringBuilder {
         val buf = BufferedReader(
                 InputStreamReader(
-                        c.inputStream))
+                        c.inputStream, Charset.forName("UTF-8")))
         val response = StringBuilder()
         var inputLine: String? = ""
 
@@ -40,6 +45,7 @@ class HttpService (){
 
         return response
     }
+
     @Throws(Exception::class)
     fun post(url: String, json: Any, timeout: Int): String {
         try {
@@ -49,7 +55,7 @@ class HttpService (){
             c.setRequestProperty("Content-Type", "application/json; utf-8")
             c.setRequestProperty("Accept", "application/json")
             c.connectTimeout = timeout
-            c.readTimeout=timeout
+            c.readTimeout = timeout
             c.doOutput = true
 
             var jsonstring = om.writeValueAsString(json)
@@ -61,6 +67,19 @@ class HttpService (){
 
             var response = readrespone(c)
             return response.toString()
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    fun get2(url: String, timeout: Int = 2000): String? {
+        val ee = Executors.newSingleThreadExecutor()
+        var t = HttpGetTask(url)
+
+        try {
+            var f = ee.submit(t)
+            return f.get(timeout.toLong(), TimeUnit.MILLISECONDS)
+
         } catch (e: Exception) {
             throw e
         }
